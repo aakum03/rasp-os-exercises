b8dd187731724e2bc3dc1e64a533cbe4e339882b
Ashwin Kumar <ashwinkumar3399@gmail.com>
2021-10-02 20:09:47 -0400

    Add completed 2.1 code

--- 2.1/src/kernel.c
+++ 2.1/src/kernel.c
@@ -2,12 +2,21 @@
 #include "utils.h"
 #include "mini_uart.h"
 
-void kernel_main(void)
+void init_kernel()
 {
 	uart_init();
 	init_printf(0, putc);
+}
+
+void el_print()
+{
 	int el = get_el();
 	printf("Exception level: %d \r\n", el);
+}
+
+void kernel_main(void)
+{
+	el_print();
 
 	while (1) {
 		uart_send(uart_recv());
